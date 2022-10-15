ad761869d6ea88088e3af3e59881c0a46434d979
/\///\/_// <omarrushil@gmail.com>
2022-10-15 22:31:26 +0530

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,45 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . 
+        . . . . . . . . 
+        . . . . . . . . 
+        . . . . . . . . 
+        . . . 7 7 . . . 
+        . . . 7 7 . . . 
+        . . . 7 7 . . . 
+        . . . 7 7 . . . 
+        `, ship, 0, -140)
+    projectile.startEffect(effects.coolRadial, 100)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    otherSprite.destroy(effects.disintegrate)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    scene.cameraShake(4, 500)
+    otherSprite.destroy(effects.disintegrate)
+    sprite.startEffect(effects.fire, 200)
+    info.changeLifeBy(-1)
+})
+let projectile: Sprite = null
+let ship: Sprite = null
+let asteroids = [
+sprites.castle.skellyFront,
+sprites.castle.skellyAttackFront1,
+sprites.castle.skellyAttackFront2,
+sprites.castle.skellyAttackFront3,
+sprites.castle.skellyWalkFront1,
+sprites.castle.skellyAttackFront4
+]
+ship = sprites.create(sprites.castle.heroWalkShieldBack1, SpriteKind.Player)
+ship.setStayInScreen(true)
+ship.bottom = 120
+controller.moveSprite(ship, 100, 100)
+info.setLife(3)
+effects.starField.startScreenEffect()
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(asteroids[randint(0, asteroids.length - 1)], 0, 75)
+    projectile.setKind(SpriteKind.Enemy)
+    projectile.x = randint(10, 150)
+})
